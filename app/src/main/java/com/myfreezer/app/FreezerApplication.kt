234a47089aefd4aced15982c6f63de3d81976bd8
BrandedNomad <info@brandednomad.com>
2022-08-13 21:10:15 +0700

    Initial: Project initial resource files, images, strings, dimensions, colors, etc. has been added

---
+++ app/src/main/java/com/myfreezer/app/FreezerApplication.kt
@@ -0,0 +1,7 @@
+package com.myfreezer.app
+
+import android.app.Application
+
+class FreezerApplication: Application() {
+
+}
---
+++ app/src/main/java/com/myfreezer/app/ui/main/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.myfreezer.app.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.myfreezer.app.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+    //my file
+
+}
